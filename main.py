00250572f2b6342696a3702eb94057a8d0176041
yusuf <127344709+yusufgulsun@users.noreply.github.com>
2023-06-02 05:48:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import insan as ins # insan.py dosyasını import ediyoruz. 
+import issiz as iss # issiz.py dosyasını import ediyoruz. 
+import calisan as cal   # calisan.py dosyasını import ediyoruz.
+import maviyaka as mavi # maviyaka.py dosyasını import ediyoruz.
+import beyazyaka as beyaz # beyazyaka.py dosyasını import ediyoruz.
+import pandas as pd # pandas kütüphanesini import ediyoruz.
+
 def main():
